ea95f7e2e9699608da17d9320d86301047bf0f9e
HuynhKhoaIT <>
2023-12-10 10:46:22 +0700

    layout

--- src/components/Product/productItem.js
+++ src/components/Product/productItem.js
@@ -1,82 +1,108 @@
-import React, { useEffect, useState } from 'react';
-import classNames from 'classnames/bind';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import styles from './product.module.scss';
-import { RatingStar } from '../Icons/Icons';
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useState } from "react";
+import classNames from "classnames/bind";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import styles from "./product.module.scss";
+import { RatingStar } from "../Icons/Icons";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const cx = classNames.bind(styles);
 function ProductItem(data) {
-    const token = localStorage.getItem('token');
-    const renderStars = (rating) => {
-        const stars = [];
-        for (let i = 0; i < rating; i++) {
-            stars.push(
-                <div className={cx('star-styles')}>
-                    <RatingStar key={i} />
-                </div>,
-            );
-        }
-
-        return stars;
-    };
-
-    const capitalizeFirstLetter = (word) => {
-        if (!word) {
-            return ''; // Return an empty string or handle the case when word is undefined or empty
-        } else return word.charAt(0).toUpperCase() + word.slice(1);
-    };
-
-    const handleAddToCartClick = async (itemId, itemQuantity = 1) => {
-        if (token) {
-            try {
-                const response = await fetch('/api/carts/add-to-cart', {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${token}`,
-                    },
-                    body: JSON.stringify({
-                        itemId: itemId,
-                        quantity: itemQuantity,
-                    }),
-                });
-
-                if (response.ok) {
-                    toast.success('Thêm thành công sản phẩm vào giỏ hàng!');
-                } else {
-                    toast.error('Có lỗi xảy ra trong quá trình thêm giỏ hàng!');
-                }
-            } catch (error) {
-                toast.error('Có lỗi xảy ra trong quá trình thêm giỏ hàng!');
-                console.error('Error:', error);
-            } finally {
-                console.log('Done!');
-            }
-        } else {
-            alert('Vui lòng đăng nhập để thực hiện chức năng này!');
-        }
-    };
-
-    return (
-        <div className={cx('product-item')}>
-            <div className={cx('product-item__img')}>
-                <img src="https://res.cloudinary.com/dawwzvnhe/image/upload/v1692778654/src/images/products/Monitor/Dell/LCD_S2421H/front1_zcl5i8.webp" />
-            </div>
-            <div className={cx('product-item__info')}>
-                <p className={cx('product-item__name')}>
-                    Laptop ACER Aspire 3 A315-59-381E (NX.K6TSV.006) (i3-1215U/RAM 8GB/512GB SSD/ Windows 11)
-                </p>
-                <div>
-                    <p className={cx('product-item__price')}>13.000.000đ</p>
-                    <p className={cx('product-item__price-sale')}>14.000.000đ</p>
-                </div>
-            </div>
+  const token = localStorage.getItem("token");
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(
+        <div className={cx("star-styles")}>
+          <RatingStar key={i} />
         </div>
-    );
+      );
+    }
+
+    return stars;
+  };
+
+  const capitalizeFirstLetter = (word) => {
+    if (!word) {
+      return ""; // Return an empty string or handle the case when word is undefined or empty
+    } else return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
+  const handleAddToCartClick = async (itemId, itemQuantity = 1) => {
+    if (token) {
+      try {
+        const response = await fetch(
+          "https://cnpmmnhom14.onrender.com/api/carts/add-to-cart",
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              itemId: itemId,
+              quantity: itemQuantity,
+            }),
+          }
+        );
+
+        if (response.ok) {
+          toast.success("Thêm thành công sản phẩm vào giỏ hàng!");
+        } else {
+          toast.error("Có lỗi xảy ra trong quá trình thêm giỏ hàng!");
+        }
+      } catch (error) {
+        toast.error("Có lỗi xảy ra trong quá trình thêm giỏ hàng!");
+        console.error("Error:", error);
+      } finally {
+        console.log("Done!");
+      }
+    } else {
+      alert("Vui lòng đăng nhập để thực hiện chức năng này!");
+    }
+  };
+  const formatCurrency = (price) => {
+    const formattedPrice = Number(price).toLocaleString("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    });
+    // Loại bỏ ký hiệu đơn vị để chỉ lấy phần số
+    const parts = formattedPrice.split(" ");
+    const priceWithoutSymbol = parts[0];
+
+    return `${priceWithoutSymbol}`;
+  };
+  return (
+    <div className={cx("product-item")}>
+      <div className={cx("product-item__img")}>
+        <img src="https://res.cloudinary.com/dawwzvnhe/image/upload/v1692778654/src/images/products/Monitor/Dell/LCD_S2421H/front1_zcl5i8.webp" />
+      </div>
+      <div className={cx("product-item__info")}>
+        <Link
+          to={`/product-detail?id=${data?.data?._id}`}
+          className={cx("product-item__name")}
+        >
+          {data?.data?._name}
+        </Link>
+        <div className={cx("product-price")}>
+          <p className={cx("product-item__price")}>
+            {formatCurrency(
+              data?.data?._price -
+                (data?.data?._price * data?.data?._salePercent) / 100
+            )}
+          </p>
+          <p className={cx("product-item__price-sale")}>
+            {formatCurrency(data?.data?._price)}
+          </p>
+        </div>
+      </div>
+      <div className={cx("product-sale")}>
+        {Math.round(data?.data?._salePercent)}%
+      </div>
+    </div>
+  );
 }
 
 export default ProductItem;
--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,95 +1,99 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import styles from './sidebar.module.scss';
-import classNames from 'classnames/bind';
-import Col from 'react-bootstrap/esm/Col';
-import ViewAll from '../view-all/view-all.js';
-import { Link } from 'react-router-dom';
+import styles from "./sidebar.module.scss";
+import classNames from "classnames/bind";
+import Col from "react-bootstrap/esm/Col";
+import ViewAll from "../view-all/view-all.js";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 function Sidebar() {
-    const [brands, setBrands] = useState([
-        {
-            _id: '',
-            _name: '',
-        },
-    ]);
+  const [brands, setBrands] = useState([
+    {
+      _id: "",
+      _name: "",
+    },
+  ]);
 
-    const [categories, setCategories] = useState([
-        {
-            _id: '',
-            _name: '',
-            status: true,
-        },
-    ]);
+  const [categories, setCategories] = useState([
+    {
+      _id: "",
+      _name: "",
+      status: true,
+    },
+  ]);
 
-    const capitalizeFirstLetter = (word) => {
-        if (!word) {
-            return ''; // Return an empty string or handle the case when word is undefined or empty
-        } else return word.charAt(0).toUpperCase() + word.slice(1);
+  const capitalizeFirstLetter = (word) => {
+    if (!word) {
+      return ""; // Return an empty string or handle the case when word is undefined or empty
+    } else return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
+  //get all brands
+  useEffect(() => {
+    const fetchAllBrands = async () => {
+      try {
+        const response = await fetch(
+          "https://cnpmmnhom14.onrender.com/api/brands"
+        );
+        if (!response.ok) {
+          throw new Error("Request failed");
+        }
+        const data = await response.json();
+        //console.log(data)
+        setBrands(data);
+      } catch (error) {
+        console.error("Không lấy được dữ liệu: ", error);
+      }
     };
+    fetchAllBrands();
+  }, []);
 
-    //get all brands
-    useEffect(() => {
-        const fetchAllBrands = async () => {
-            try {
-                const response = await fetch('/api/brands');
-                if (!response.ok) {
-                    throw new Error('Request failed');
-                }
-                const data = await response.json();
-                //console.log(data)
-                setBrands(data);
-            } catch (error) {
-                console.error('Không lấy được dữ liệu: ', error);
-            }
-        };
-        fetchAllBrands();
-    }, []);
+  //get all categories
+  useEffect(() => {
+    const fetchAllCagtegories = async () => {
+      try {
+        const response = await fetch(
+          "https://cnpmmnhom14.onrender.com/api/categories/"
+        );
+        if (!response.ok) {
+          throw new Error("Request failed");
+        }
+        const data = await response.json();
+        //console.log(data)
+        setCategories(data);
+      } catch (error) {
+        console.error("Không lấy được dữ liệu: ", error);
+      }
+    };
+    fetchAllCagtegories();
+  }, []);
 
-    //get all categories
-    useEffect(() => {
-        const fetchAllCagtegories = async () => {
-            try {
-                const response = await fetch('/api/categories/');
-                if (!response.ok) {
-                    throw new Error('Request failed');
-                }
-                const data = await response.json();
-                //console.log(data)
-                setCategories(data);
-            } catch (error) {
-                console.error('Không lấy được dữ liệu: ', error);
-            }
-        };
-        fetchAllCagtegories();
-    }, []);
+  return (
+    <div className={cx("sidebar-container")}>
+      <div className={cx("side-bar-wrapper")}>
+        <nav class="category">
+          <h3 className={cx("category__heading")}>
+            <i className={cx("heading__icon fa-solid fa-list")}></i>Danh mục
+          </h3>
+          <ul className={cx("category-list")}>
+            {categories.map((category) => (
+              <li className={cx("category-item")}>
+                <Link
+                  key={category._id}
+                  to={`/search?keyword=${category._name}`}
+                  state={{ keyId: `${category._id}` }}
+                  className={cx("category-item__link")}
+                >
+                  {capitalizeFirstLetter(category._name)}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
 
-    return (
-        <div className={cx('sidebar-container')}>
-            <div className={cx('side-bar-wrapper')}>
-                <nav class="category">
-                    <h3 className={cx('category__heading')}>
-                        <i className={cx('heading__icon fa-solid fa-list')}></i>Danh mục
-                    </h3>
-                    <ul className={cx('category-list')}>
-                        {categories.map((category) => (
-                            <li className={cx('category-item')}>
-                                <Link
-                                    key={category._id}
-                                    to={`/search?keyword=${category._name}`}
-                                    state={{ keyId: `${category._id}` }}
-                                    className={cx('category-item__link')}
-                                >
-                                    {capitalizeFirstLetter(category._name)}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </div>
-
-            {/* <div className={cx('side-bar-wrapper')}>
+      {/* <div className={cx('side-bar-wrapper')}>
                 <nav class="category">
                     <h3 className={cx('category__heading')}><i className={cx('heading__icon fa-solid fa-list')}></i>Hãng</h3>
                     <ul className={cx('category-list')}>
@@ -110,7 +114,7 @@ function Sidebar() {
                     </ul>
                 </nav>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 }
 export default Sidebar;
